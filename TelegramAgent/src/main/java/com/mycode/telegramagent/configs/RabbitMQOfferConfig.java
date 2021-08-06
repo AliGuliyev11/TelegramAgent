ab706e6bdd9b8c81257149e38899e7016b7e64c0
AliGuliyev11 <aliug@code.edu.az>
2021-08-06 21:14:56 +0400

    Heroku deply

--- TelegramAgent/src/main/java/com/mycode/telegramagent/configs/RabbitMQOfferConfig.java
+++ TelegramAgent/src/main/java/com/mycode/telegramagent/configs/RabbitMQOfferConfig.java
@@ -1,4 +1,4 @@
-package com.mycode.telegramagent.rabbitmq.offerOrder.publisher;
+package com.mycode.telegramagent.configs;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -9,6 +9,7 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author Ali Guliyev
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
  * @implNote This configuration class configure queue,bind,exchange of publisher
  */
 
+@Profile("dev")
 @Configuration(value = "offerConfig")
 public class RabbitMQOfferConfig {
 
@@ -42,6 +44,30 @@ public class RabbitMQOfferConfig {
     @Bean(name = "offerBind")
     Binding offerBind(@Qualifier("offerQueue") Queue queue, @Qualifier("offerExchange") DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(OFFER_KEY);
+    }
+
+    public static final String ORDER_QUEUE = "orderQueue";
+
+    public static final String STOP_ORDER_QUEUE = "stopOrderQueue";
+    public static final String OFFER_REPLY_QUEUE = "offerReplyQueue";
+
+
+    @Bean
+    Queue rabbitQueue() {
+        Queue orderQueue = new Queue(ORDER_QUEUE, true);
+        return orderQueue;
+    }
+
+    @Bean
+    Queue stopRabbitQueue() {
+        Queue orderQueue = new Queue(STOP_ORDER_QUEUE, true);
+        return orderQueue;
+    }
+
+    @Bean
+    Queue offerReplyQueue() {
+        Queue orderQueue = new Queue(OFFER_REPLY_QUEUE, true);
+        return orderQueue;
     }
 
 
--- TelegramAgent/src/main/java/com/mycode/telegramagent/configs/production/RabbitMQOfferConfig.java
+++ TelegramAgent/src/main/java/com/mycode/telegramagent/configs/production/RabbitMQOfferConfig.java
@@ -1,14 +1,21 @@
-package com.mycode.telegramagent.rabbitmq.offerOrder.publisher;
+package com.mycode.telegramagent.configs.production;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author Ali Guliyev
@@ -16,6 +23,7 @@ import org.springframework.context.annotation.Configuration;
  * @implNote This configuration class configure queue,bind,exchange of publisher
  */
 
+@Profile("!dev")
 @Configuration(value = "offerConfig")
 public class RabbitMQOfferConfig {
 
@@ -45,11 +53,6 @@ public class RabbitMQOfferConfig {
     }
 
 
-    @Bean
-    MessageConverter offerMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
     @Bean(name = "offerMadeQueue")
     Queue offerMadeQueue() {
         Queue orderQueue = new Queue(OFFER_MADE_QUEUE, true);
@@ -64,6 +67,51 @@ public class RabbitMQOfferConfig {
     @Bean(name = "offerMadeBind")
     Binding offerMadeBind(@Qualifier("offerMadeQueue") Queue queue, @Qualifier("offerMadeExchange") DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(OFFER_MADE_KEY);
+    }
+
+    public static final String ORDER_QUEUE = "orderQueue";
+
+    public static final String STOP_ORDER_QUEUE = "stopOrderQueue";
+    public static final String OFFER_REPLY_QUEUE = "offerReplyQueue";
+
+
+    @Bean
+    Queue rabbitQueue() {
+        Queue orderQueue = new Queue(ORDER_QUEUE, true);
+        return orderQueue;
+    }
+
+    @Bean
+    Queue stopRabbitQueue() {
+        Queue orderQueue = new Queue(STOP_ORDER_QUEUE, true);
+        return orderQueue;
+    }
+
+    @Bean
+    Queue offerReplyQueue() {
+        Queue orderQueue = new Queue(OFFER_REPLY_QUEUE, true);
+        return orderQueue;
+    }
+
+
+    @Bean
+    public ConnectionFactory connectionFactory() throws URISyntaxException {
+        final URI rabbitMqUrl = new URI(System.getenv("CLOUDAMQP_URL"));
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUri(rabbitMqUrl);
+        return factory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() throws URISyntaxException {
+        RabbitTemplate temp = new RabbitTemplate(connectionFactory());
+        temp.setMessageConverter(offerMessageConverter());
+        return temp;
+    }
+
+    @Bean
+    MessageConverter offerMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
